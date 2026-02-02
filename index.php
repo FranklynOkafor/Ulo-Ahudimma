93b799ab2a5383574e83dba739a7e36922663e7d
Emeka <frankeokafor@gmail.com>
2026-02-02 22:41:32 +0100

    Made some bitty changes

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
 <?php
-  echo 'index is active';
+// Main template file
+// Fallback for all content types
+
+get_header();
 ?>
+
+<main id="primary" class="site-main">
+  <div class="container">
+
+    <?php
+    if (have_posts()) :
+      while (have_posts()) :
+
+        the_post();
+
+        get_template_part(
+          'template-parts/content/content',
+          get_post_type()
+        );
+
+      endwhile;
+
+      the_posts_pagination();
+    else :
+      get_template_part('template-parts/content/content', 'none');
+    endif;
+    ?>
+
+  </div>
+</main>
+
+<?php
+get_footer();
