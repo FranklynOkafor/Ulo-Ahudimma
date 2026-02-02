48c1b1305b75be7df0e9c49da729bad9bfe5f338
Emeka <frankeokafor@gmail.com>
2026-02-02 21:40:48 +0100

    First commit, just did the basics. Theme structure, added details in style.css, added hooks in functions.php

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+// Theme Setup
+require_once get_template_directory() . '/inc/setup.php';
+require_once get_template_directory() . '/inc/enqueue.php';
+
+// Custom Post Type 
+require_once get_template_directory() . '/inc/cpt/doctors.php';
+require_once get_template_directory() . '/inc/cpt/departments.php';
+require_once get_template_directory() . '/inc/cpt/appointments.php';
+
+
+require_once get_template_directory() . '/inc/customizer/colors.php';
+require_once get_template_directory() . '/inc/customizer/typography.php';
+require_once get_template_directory() . '/inc/customizer/layout.php';
