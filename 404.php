53d493360a6a68d38629a02fc5278938a0a0203b
Emeka <frankeokafor@gmail.com>
2026-02-16 23:57:13 +0100

    Polished the 404 page

--- 404.php
+++ 404.php
@@ -1,3 +1,6 @@
-<?php
-  echo '404 page is active';
-?>
+<?php get_header(); ?>
+  <div style="margin-top: 50px;" class="container">
+    <h1>Sorry your page was not found</h1>
+    <p>Check the url and try again</p>
+  </div>
+<?php get_footer(); ?>
