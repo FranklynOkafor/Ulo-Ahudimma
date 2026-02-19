f6ac8d32f7f410fc10e2daf8219b9aa55205605b
Emeka <frankeokafor@gmail.com>
2026-02-19 20:23:00 +0100

    Initial version

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
   <div style="margin-top: 50px;" class="container">
-    <h1>Sorry your page was not found</h1>
+    <h1>Sorry your page was not found.</h1>
     <p>Check the url and try again</p>
   </div>
 <?php get_footer(); ?>
